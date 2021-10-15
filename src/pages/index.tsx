e7d54818a3f56f836f3bbbb0db62740560a80d65
Rafael <alvesrafael17@hotmail.com>
2021-10-15 15:02:31 -0300

    Pagina inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import styles from '../styles/global.module.scss';
-import Head from 'next/head';
+import styles from "./home.module.scss";
+import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
 
 export default function Home() {
   return (
@@ -7,9 +8,20 @@ export default function Home() {
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      <h1>
-        Hello <span>Next</span>
-      </h1>
+      <main className={styles.container}>
+        <section className={styles.hero}>
+          <span>👏 Olá, bem-vindo(a)</span>
+          <h1>
+            Novidades sobre o mundo do <span>React</span>. <br />
+          </h1>
+          <p>
+            Tenha acesso a todas as postagens
+            <span> por R$9,99/mês</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Garota desenvolvendo" />
+      </main>
     </>
   );
 }
