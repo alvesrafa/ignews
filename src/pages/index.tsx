05e84f3b1e07c36ab5d59807070f4014785b2e0d
Rafael <alvesrafael17@hotmail.com>
2021-10-15 16:25:51 -0300

    chamada server sider rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
+import { GetServerSideProps } from "next";
+
 import styles from "./home.module.scss";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -16,12 +26,30 @@ export default function Home() {
           </h1>
           <p>
             Tenha acesso a todas as postagens
-            <span> por R$9,99/mês</span>
+            <span> por {product.amount}/mês</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Garota desenvolvendo" />
       </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1JkvFSDtn5sEhF6Wjju3omyx", {
+    expand: ["product"],
+  });
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(price.unit_amount / 100),
+  };
+  return {
+    props: {
+      product,
+    },
+  };
+};
